d8521c07015663243a35178f7e509ffaf6e1176b
Ziwei Xu <zix1@student.unimelb.edu.au>
2020-03-13 15:37:34 +1100

    Updates

---
+++ precache-manifest.a5aae45b06f3ec412b6af6fee97bea80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96c6834247b631183cb65d16043d966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adb2948eb27356e499d1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "741b22d8d3502dc7f023",
+    "url": "/static/js/2.03c91233.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.03c91233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb2948eb27356e499d1",
+    "url": "/static/js/main.992ed6ef.chunk.js"
+  },
+  {
+    "revision": "085c32a89ba91a7c2d7f",
+    "url": "/static/js/runtime-main.ce3e6c9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
