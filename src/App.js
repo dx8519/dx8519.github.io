3fb72ac69a7ba8898096061b5c21784a221b2f41
Ziwei Xu <zix1@student.unimelb.edu.au>
2020-03-15 15:05:30 +1100

    Appbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Home";
+import {AppBar, Button, createMuiTheme, Toolbar, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import {ThemeProvider} from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    boxShadow: "none",
+  },
+  title: {
+    flexGrow: 1,
+    fontWeight: 700,
+    letterSpacing: "0.06em",
+
+  },
+  toolBar: {
+    width: "50%",
+    minWidth: 320,
+    maxWidth: 1080,
+    margin: "auto"
+  }
+}));
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary:{
+      main:"#282c34"
+    },
+  },
+  typography: {
+    fontFamily: [
+      "monospace"
+    ],
+    fontSize:16
+
+
+  }
+});
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. hi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <nav>
+          <AppBar position="absolute" className={classes.appBar}>
+            <Toolbar className={classes.toolBar}>
+              <Typography className={classes.title}>
+                DX8519
+              </Typography>
+              <Button>About</Button>
+              <Button>CV</Button>
+            </Toolbar>
+          </AppBar>
+        </nav>
+        <main>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </main>
+      </ThemeProvider>
+
+
+
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+
+export default function Home(props) {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
